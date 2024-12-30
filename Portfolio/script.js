a94f4b02a1dfd19b8d4ba33fcb687d972b139260
Stan45E <70811059+Stan45E@users.noreply.github.com>
2024-12-30 22:19:05 +0530

    Delete Portfolio/script.js

--- Portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// Add any interactivity here (e.g., smooth scrolling, animations, etc.)
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
