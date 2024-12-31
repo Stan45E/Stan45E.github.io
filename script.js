60ec2ecccf067926759e6f4fb2efe312c03d6738
Stan45E <70811059+Stan45E@users.noreply.github.com>
2024-12-31 10:36:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Add interactivity such as smooth scrolling or animations if needed
-document.querySelectorAll('nav a').forEach(anchor => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
